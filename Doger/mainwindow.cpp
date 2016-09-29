47c498761d50bb72fa31407142af215f93ca6fed
Julien Delvaux <delvaux.ju@gmail.com>
2016-09-29 19:26:52 +0200

    Add the possibility to insert/modify/remove an item in the itemlist

--- Doger/mainwindow.cpp
+++ Doger/mainwindow.cpp
@@ -6,26 +6,30 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    itemwindow = new Itemwindow;
     listwindow = new Listwindow;
     statisticswindow = new Statisticswindow;
+    sqlite = new SqLite;
 
     ui->stackedWidget->setCurrentIndex(0);
     ui->frame_title->hide();
     ui->btn_config_categorie_delete->hide();
     ui->btn_config_categorie_save->hide();
-    ui->le_config_categorie->hide();
+    ui->le_config_categoryName->hide();
     ui->btn_config_brand_delete->hide();
     ui->btn_config_brand_save->hide();
-    ui->le_config_brand->hide();
+    ui->le_config_brandName->hide();
 
     loadConfig();
+    loadDatabase();
     initActionsConnections();
+
 
 }
 
 MainWindow::~MainWindow()
 {
+    sqlite->closeDB();
+    delete sqlite;
     delete ui;
 }
 
@@ -59,6 +63,38 @@ void MainWindow::loadConfig()
     else{
         QMessageBox::warning(this, QGuiApplication::applicationDisplayName(),tr("Impossible de charger \"config.ini\"."));
     }
+}
+
+void MainWindow::loadDatabase(){
+
+    sqlite->openDB("database.db");
+    refreshDatabase();
+
+}
+
+void MainWindow::refreshDatabase(){
+
+    sqlite->getCategoryBrand(CATEGORY, &idCategories, ui->cb_config_categories);
+    sqlite->getCategoryBrand(BRAND, &idBrand, ui->cb_config_brand);
+    ui->cb_config_categories->insertItem(0,"Nouvelle");
+    ui->cb_config_brand->insertItem(0,"Nouvelle");
+
+    QString req="SELECT Items.id_item, Categories.name, Brands.name, Items.reference, Items.weight, Items.quantity, Items.desired FROM Brands, Categories INNER JOIN Items ON Items.id_category = Categories.id_category AND Items.id_brand = Brands.id_brand WHERE Brands.id_brand = Items.id_brand AND Categories.id_category = Categories.id_category";
+    QSqlQueryModel *model = new QSqlQueryModel();
+    model->setQuery(req);
+    model->setHeaderData(1, Qt::Horizontal, tr("Catégorie"));
+    model->setHeaderData(2, Qt::Horizontal, tr("Marque"));
+    model->setHeaderData(3, Qt::Horizontal, tr("Modèle"));
+    model->setHeaderData(4, Qt::Horizontal, tr("Poids"));
+    model->setHeaderData(5, Qt::Horizontal, tr("Quantité"));
+    model->setHeaderData(6, Qt::Horizontal, tr("Liste de souhait"));
+
+    QSortFilterProxyModel *filter = new QSortFilterProxyModel();
+    filter->setSourceModel(model);
+
+    ui->tw_itemsList->setModel(filter);
+    ui->tw_itemsList->setSortingEnabled(true);
+    //ui->tw_itemsList->hideColumn(0);
 }
 
 void MainWindow::on_btn_home_clicked()
@@ -103,21 +139,23 @@ void MainWindow::on_btn_about_clicked()
     ui->lbl_title->setText("A propos");
 }
 
-void MainWindow::initActionsConnections()
-{
-    //connect(ui->btn_materiel_add, SIGNAL(clicked()), placard, SLOT(show()));
-
-}
 void MainWindow::on_btn_materiel_add_clicked()
 {
-    itemwindow->show();
-    itemwindow->setPage(PAGE_MATERIEL);
+    ItemWindow *itemwindow = new ItemWindow(0,sqlite,0);
+    if(!itemwindow->exec())
+    {
+        itemwindow->setPage(PAGE_MATERIEL);
+        itemwindow->show();
+    }
+    delete itemwindow;
+    refreshDatabase();
+
 }
 
 void MainWindow::on_btn_alimentation_add_clicked()
 {
-    itemwindow->show();
-    itemwindow->setPage(PAGE_ALIMENTATION);
+    /*itemwindow->show();
+    itemwindow->setPage(PAGE_ALIMENTATION);*/
 }
 
 
@@ -138,14 +176,14 @@ void MainWindow::on_cb_config_categories_activated(int index)
     if(index==0){
         ui->btn_config_categorie_delete->hide();
         ui->btn_config_categorie_save->show();
-        ui->le_config_categorie->show();
-        ui->le_config_categorie->setText("");
+        ui->le_config_categoryName->show();
+        ui->le_config_categoryName->setText("");
     }
     else{
         ui->btn_config_categorie_delete->show();
         ui->btn_config_categorie_save->show();
-        ui->le_config_categorie->show();
-        ui->le_config_categorie->setText(ui->cb_config_categories->itemText(index));
+        ui->le_config_categoryName->show();
+        ui->le_config_categoryName->setText(ui->cb_config_categories->itemText(index));
     }
 }
 
@@ -154,44 +192,129 @@ void MainWindow::on_cb_config_brand_activated(int index)
     if(index==0){
         ui->btn_config_brand_delete->hide();
         ui->btn_config_brand_save->show();
-        ui->le_config_brand->show();
-        ui->le_config_brand->setText("");
+        ui->le_config_brandName->show();
+        ui->le_config_brandName->setText("");
     }
     else{
         ui->btn_config_brand_delete->show();
         ui->btn_config_brand_save->show();
-        ui->le_config_brand->show();
-        ui->le_config_brand->setText(ui->cb_config_categories->itemText(index));
+        ui->le_config_brandName->show();
+        ui->le_config_brandName->setText(ui->cb_config_brand->itemText(index));
     }
 
 }
 
-void MainWindow::on_actionQuitter_triggered()
+void MainWindow::initActionsConnections()
 {
-    close();
+    connect(ui->actionA_propos, SIGNAL(triggered()), this, SLOT(on_btn_about_clicked()));
+    connect(ui->actionConfiguration, SIGNAL(triggered()), this, SLOT(on_btn_config_clicked()));
+    connect(ui->actionPlacard, SIGNAL(triggered()), this, SLOT(on_btn_materiel_clicked()));
+    connect(ui->actionListes, SIGNAL(triggered()), this, SLOT(on_btn_liste_clicked()));
+    connect(ui->actionPlans_alimentation, SIGNAL(triggered()), this, SLOT(on_btn_alimentation_clicked()));
+    connect(ui->actionQuitter, SIGNAL(triggered()), this, SLOT(close()));
 }
 
-void MainWindow::on_actionConfiguration_triggered()
+
+void MainWindow::on_btn_config_categorie_save_clicked()
 {
-    on_btn_config_clicked();
+    QString text = "";
+    if(ui->cb_config_categories->currentIndex()==0){
+        sqlite->addCategoryBrand(CATEGORY,ui->le_config_categoryName->text());
+        text="Ajout d'une catégorie avec succès.";
+    }
+    else{
+        sqlite->modifyCategory(idCategories.at(ui->cb_config_categories->currentIndex()-1),ui->le_config_categoryName->text());
+        text="Modification d'une catégorie avec succès.";
+    }
+
+    ui->btn_config_categorie_delete->hide();
+    ui->btn_config_categorie_save->hide();
+    ui->le_config_categoryName->hide();
+
+    refreshDatabase();
+
+    ui->statusBar->showMessage(text, 3000);
 }
 
-void MainWindow::on_actionA_propos_triggered()
+void MainWindow::on_btn_config_categorie_delete_clicked()
 {
-    on_btn_about_clicked();
+    sqlite->deleteCategoryBrand(CATEGORY, idCategories.at(ui->cb_config_categories->currentIndex()-1));
+
+    ui->btn_config_categorie_delete->hide();
+    ui->btn_config_categorie_save->hide();
+    ui->le_config_categoryName->hide();
+
+    refreshDatabase();
+
+    ui->statusBar->showMessage("Catégorie supprimée avec succès.", 3000);
 }
 
-void MainWindow::on_actionPlacard_triggered()
+void MainWindow::on_btn_config_brand_save_clicked()
 {
-    on_btn_materiel_clicked();
+    QString text = "";
+    if(ui->cb_config_brand->currentIndex()==0){
+        sqlite->addCategoryBrand(BRAND, ui->le_config_brandName->text());
+        text="Ajout d'une marque avec succès.";
+    }
+    else{
+        sqlite->modifyBrand(idBrand.at(ui->cb_config_brand->currentIndex()-1),ui->le_config_brandName->text());
+        text="Modification d'une marque avec succès.";
+    }
+
+    ui->btn_config_brand_delete->hide();
+    ui->btn_config_brand_save->hide();
+    ui->le_config_brandName->hide();
+
+    refreshDatabase();
+
+    ui->statusBar->showMessage(text, 3000);
 }
 
-void MainWindow::on_actionListes_triggered()
+void MainWindow::on_btn_config_brand_delete_clicked()
 {
-    on_btn_liste_clicked();
+    sqlite->deleteCategoryBrand(BRAND, idBrand.at(ui->cb_config_brand->currentIndex()-1));
+
+    ui->btn_config_brand_delete->hide();
+    ui->btn_config_brand_save->hide();
+    ui->le_config_brandName->hide();
+
+    refreshDatabase();
+
+    ui->statusBar->showMessage("Catégorie supprimée avec succès.", 3000);
 }
 
-void MainWindow::on_actionPlans_alimentation_triggered()
+void MainWindow::on_tw_itemsList_doubleClicked(const QModelIndex &index)
 {
-    on_btn_alimentation_clicked();
+    ItemWindow *itemwindow = new ItemWindow(0,sqlite, index.sibling(index.row(),0).data().toInt() );
+    if(!itemwindow->exec())
+    {
+        itemwindow->setPage(PAGE_MATERIEL);
+        itemwindow->show();
+    }
+    delete itemwindow;
+
+    refreshDatabase();
+}
+
+void MainWindow::on_btn_materiel_delete_clicked()
+{
+    int reponse = QMessageBox::question(this, "Supression?", tr("Supprimer %1 ?")
+                                        .arg(ui->tw_itemsList->currentIndex().sibling(ui->tw_itemsList->currentIndex().row(),3).data().toString())
+                                        ,QMessageBox::Yes | QMessageBox::No);
+
+            if(reponse == QMessageBox::Yes){
+                QString Squery = "DELETE FROM Items WHERE id_item=:id_item";
+                QSqlQuery query;
+                query.prepare(Squery);
+                query.bindValue(":id_item", ui->tw_itemsList->currentIndex().sibling(ui->tw_itemsList->currentIndex().row(),0).data().toString());
+
+                if(!query.exec()) qDebug()<<"[SQLite] Erreur lors de la suppression d'un item.";
+            }
+     refreshDatabase();
+
+}
+
+void MainWindow::on_btn_materiel_modify_clicked()
+{
+    on_tw_itemsList_doubleClicked(ui->tw_itemsList->currentIndex());
 }
--- Doger/mainwindow.h
+++ Doger/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "sqlite.h"
+#include <QMessageBox>
 #include "itemwindow.h"
 #include "listwindow.h"
 #include "statisticswindow.h"
@@ -20,6 +22,8 @@ public:
 
 private slots:
     void loadConfig();
+    void loadDatabase();
+    void refreshDatabase();
     void initActionsConnections();
     void on_btn_materiel_clicked();
 
@@ -45,23 +49,30 @@ private slots:
 
     void on_cb_config_brand_activated(int index);
 
-    void on_actionQuitter_triggered();
+    void on_btn_config_categorie_save_clicked();
 
-    void on_actionConfiguration_triggered();
+    void on_btn_config_categorie_delete_clicked();
 
-    void on_actionA_propos_triggered();
+    void on_btn_config_brand_save_clicked();
 
-    void on_actionPlacard_triggered();
+    void on_btn_config_brand_delete_clicked();
+    void on_tw_itemsList_doubleClicked(const QModelIndex &index);
 
-    void on_actionListes_triggered();
+    void on_btn_materiel_delete_clicked();
 
-    void on_actionPlans_alimentation_triggered();
+    void on_btn_materiel_modify_clicked();
+
+public:
+
 
 private:
     Ui::MainWindow *ui;
-    Itemwindow *itemwindow;
     Listwindow *listwindow;
     Statisticswindow *statisticswindow;
+    QVector<int> idCategories;
+    QVector<int> idBrand;
+    SqLite *sqlite;
+    QMenu* contextMenu;
 };
 
 #endif // MAINWINDOW_H
